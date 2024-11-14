9394ba0a1af55b9c812f8a39573dda4dd0ec76a2
Jun279 <33.beautifulboy@gmail.com>
2024-11-15 01:14:09 +0900

    ✨FEAT. 전통시장 API Complete

--- frontend/src/entities/traditionalMarket/main/MarketContent.jsx
+++ frontend/src/entities/traditionalMarket/main/MarketContent.jsx
@@ -2,43 +2,87 @@ import styled from "styled-components";
 import MarketTitle from "./MarketTitle";
 import List from "./List";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { selectedTradMarketState, tradMarketState } from "../../../shared/state/Market";
+import {
+  selectedTradMarketState,
+  tradMarketState,
+} from "../../../shared/state/Market";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { dummyToken } from "../../../shared/state/token";
+import { defaultApi } from "../../../apis/utils/Instance";
 
 export default function MarketContent() {
+  const [_, setSelectedTradMarket] = useRecoilState(selectedTradMarketState);
+  const navigate = useNavigate("");
+  const [option, setOption] = useState("가까운순");
+  const [isOpen, setIsOpen] = useState(false);
+  const token = useRecoilValue(dummyToken);
+  const [market, setMarket] = useState([]);
 
-    const markets = useRecoilValue(tradMarketState);
-    const [_,setSelectedTradMarket] = useRecoilState(selectedTradMarketState);
-    const navigate = useNavigate("");
+  const handleOptionClick = (selectedOption) => {
+    setOption(selectedOption);
+    setIsOpen(false);
+  };
 
+  useEffect(() => {
+    let filter;
+    if (option === "가까운순") {
+      filter = 0;
+    } else if (option === "상품 많은 순") {
+      filter = 1;
+    }
 
-    const handleMarketClick = (index) => {
-        setSelectedTradMarket(index);
-        navigate('/traditional-detail');
+    const getTraditionalMarketHandler = async () => {
+      console.log(filter);
+      try {
+        const response = await defaultApi.get(`/api/shop/traditional/sorted`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            sort: filter,
+          },
+        });
+        console.log(response);
+        if (response.status === 200) {
+          setMarket(response.data.shopInList);
+        }
+      } catch (e) {
+        console.log(e);
+      }
     };
 
-    return (
-        <Wrapper>       
-            <MarketTitle/>
-            <ListContainer>
-                {markets.map((market, index) => (
-                    <List key={index} name={market.name} img={market.img}
-                    onClick={() => handleMarketClick(index)}
-                    />
-                ))}
-            </ListContainer>
-        </Wrapper>
-    );
+    getTraditionalMarketHandler();
+  }, [option]);
+
+  const handleMarketClick = (index) => {
+    setSelectedTradMarket(index);
+    navigate("/traditional-detail");
+  };
+
+  return (
+    <Wrapper>
+      <MarketTitle option={option} isOpen={isOpen} setIsOpen={setIsOpen} handleOptionClick={handleOptionClick} />
+      <ListContainer>
+        {market.map((market, index) => (
+          <List
+            key={index}
+            item={market}
+            onClick={() => handleMarketClick(index)}
+          />
+        ))}
+      </ListContainer>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-    padding: 1.5rem 1rem;
+  padding: 1.5rem 1rem;
 `;
 
-
 const ListContainer = styled.div`
-margin-top: 1.25rem;
-display: flex;
-flex-direction: column;
-gap: 0.5rem;
-`
+  margin-top: 1.25rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
--- frontend/src/entities/traditionalMarket/main/MarketTitle.jsx
+++ frontend/src/entities/traditionalMarket/main/MarketTitle.jsx
@@ -1,97 +1,93 @@
 import styled from "styled-components";
-import icon from '../../../assets/TraditionalMarket/dropdown.svg';
+import icon from "../../../assets/TraditionalMarket/dropdown.svg";
 import check from "../../../assets/TraditionalMarket/check.svg";
-import { useState } from 'react';
 
-export default function MarketTitle(){
-    const [option, setOption] = useState("가까운순");
-    const [isOpen, setIsOpen] = useState(false);
+export default function MarketTitle({option, isOpen, setIsOpen, handleOptionClick}) {
 
-    const handleOptionClick = (selectedOption) => {
-        setOption(selectedOption);
-        setIsOpen(false);
-    };
-    return(
-        <>
-        <Title>전통시장</Title>
-        <Subtitle>위치를 기반으로 5km 이내에 있는 시장들이에요.</Subtitle>
-        <Dropdown onClick={() => setIsOpen(!isOpen)}>
-                <Option>{option}</Option>
-                <Icon src={icon} />
-            </Dropdown>
+  return (
+    <>
+      <Title>전통시장</Title>
+      <Subtitle>위치를 기반으로 5km 이내에 있는 시장들이에요.</Subtitle>
+      <Dropdown onClick={() => setIsOpen(!isOpen)}>
+        <Option>{option}</Option>
+        <Icon src={icon} />
+      </Dropdown>
 
-            {isOpen && (
-                <OptionsList>
-                    <OptionItem onClick={() => handleOptionClick("가까운순")} style={{borderBottom:"0.0625rem solid #C2C2C2"}}>
-                        <CheckIcon src={check} visible={option === "가까운순"} /> 가까운순
-                    </OptionItem>
-                    <OptionItem onClick={() => handleOptionClick("상품 많은 순")}>
-                        <CheckIcon src={check} visible={option === "상품 많은 순"} /> 상품 많은 순
-                    </OptionItem>
-                </OptionsList>
-            )}
-        </>
-        );
+      {isOpen && (
+        <OptionsList>
+          <OptionItem
+            onClick={() => handleOptionClick("가까운순")}
+            style={{ borderBottom: "0.0625rem solid #C2C2C2" }}
+          >
+            <CheckIcon src={check} visible={option === "가까운순"} /> 가까운순
+          </OptionItem>
+          <OptionItem onClick={() => handleOptionClick("상품 많은 순")}>
+            <CheckIcon src={check} visible={option === "상품 많은 순"} /> 상품
+            많은 순
+          </OptionItem>
+        </OptionsList>
+      )}
+    </>
+  );
 }
 
 const Title = styled.div`
-    color: #333;
-    font-family: Pretendard;
-    font-size: 1.25rem;
-    font-weight: 600;
+  color: #333;
+  font-family: Pretendard;
+  font-size: 1.25rem;
+  font-weight: 600;
 `;
 
 const Subtitle = styled(Title)`
-    font-size: 1rem;
-    font-weight: 400;
-    margin-top: 1rem;
+  font-size: 1rem;
+  font-weight: 400;
+  margin-top: 1rem;
 `;
 
-
 const Dropdown = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 0.25rem;
-    margin-top: 0.5rem;
-    color: #999;
-    font-family: Pretendard;
-    font-size: 0.875rem;
-    cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  margin-top: 0.5rem;
+  color: #999;
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  cursor: pointer;
 `;
 
 const Option = styled.div``;
 
 const Icon = styled.img`
-    width: 0.6875rem;
-    height: 0.375rem;
+  width: 0.6875rem;
+  height: 0.375rem;
 `;
 
 const OptionsList = styled.div`
-    position: absolute;
-    padding: 0.5rem;
-    margin-top: 0.13rem;
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-    z-index: 1;
-    border-radius: 0.5rem;
-    background: #FFF;
+  position: absolute;
+  padding: 0.5rem;
+  margin-top: 0.13rem;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+  border-radius: 0.5rem;
+  background: #fff;
 `;
 
 const OptionItem = styled.div`
-    color: #333;
-    font-family: Pretendard;
-    font-size: 1rem;
-    font-weight: 500;
-    padding: 0.5rem 1.5rem 0.5rem 0;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
+  color: #333;
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-weight: 500;
+  padding: 0.5rem 1.5rem 0.5rem 0;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 `;
 
 const CheckIcon = styled.img`
-    width: 1.125rem;
-    height: 1.125rem;
-    visibility: ${({ visible }) => (visible ? "visible" : "hidden")};
+  width: 1.125rem;
+  height: 1.125rem;
+  visibility: ${({ visible }) => (visible ? "visible" : "hidden")};
 `;
